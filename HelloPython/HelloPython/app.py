f2a7dd602a4fde179353d1d5f651d4d09babbc21
Robert <AQ_ROBY@YAHOO.COM>
2022-01-26 18:09:46 +0200

    Add more routes

--- HelloPython/HelloPython/app.py
+++ HelloPython/HelloPython/app.py
@@ -10,10 +10,21 @@ app = Flask(__name__)
 # and add decorators to define the appropriate resource locators for them.
 
 @app.route('/')
+def index():
+    return "Index!"
+
 @app.route('/hello')
 def hello():
     # Render the page
     return "Hello Python!"
+
+@app.route("/members")
+def members():
+    return "Members"
+
+@app.route("/members/<string:name>/")
+def getMember(name):
+    return name
 
 if __name__ == '__main__':
     # Run the app server on localhost:4449
